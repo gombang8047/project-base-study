65f6c73aab3a4eb46747c0dae3f0c284de9e2473
신지웅 <gombang8047@gmail.com>
2025-12-15 15:52:23 +0900

    DB and Backend setting Complete

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.enableCors({
+    // "이 출처는 믿을 수 있어. 허용해!"
+    origin: 'http://localhost:3000',
+    // "쿠키도 같이 보내도 돼!"
+    credentials: true,
+  });
+
+  await app.listen(process.env.PORT || 8000);
 }
-bootstrap();
+bootstrap().catch((err) => console.error(err));
